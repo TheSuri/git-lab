c0156bcdab23b07560a92a6caa3bf3431f0b2db9
Julia Lin <julialin@usc.edu>
2014-04-02 13:43:05 -0700

    Added rule for Buzz

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -4,7 +4,11 @@ int main(int argc, char *argv[])
 {
   for (unsigned int i = 0; i < 100; i++)
     {
-      std::cout << i << std:endl;
+      std::cout << i << std::endl;
+      if (i%5 == 0)
+	{
+	  std::cout << "Buzz" << std::endl;
+	}
     }
   return (0);
 }
